231c6e577fdab2b93215087ba8d1fab4f546523f
chartchai21 <chartchai21@gmail.com>
2020-07-16 14:40:31 +0700

    test

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 import Vue from 'vue'
+import './plugins/axios'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import '@babel/polyfill'
+import router from './router'
+import store from './store'
 
 Vue.config.productionTip = false
 
 new Vue({
   vuetify,
+  router,
+  store,
   render: h => h(App)
 }).$mount('#app')
